8cbb2969435d5bea2847e448f261be7c169dfc37
Ertan Bozkır <ertanbozkr@gmail.com>
2016-03-02 15:11:26 +0200

    İki sayı arasındaki sayıların toplamı

---
+++ while örnek/form.php
@@ -0,0 +1,15 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Başlıksız Belge</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="whileornek.php">
+  <textarea name="s1" id="s1"></textarea>
+  <textarea name="s2" id="s2"></textarea>
+  <input type="submit" name="b" id="b" value="Gönder">
+</form>
+</body>
+</html>
---
+++ while örnek/whileornek.php
@@ -0,0 +1,22 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Başlıksız Belge</title>
+</head>
+
+<body>
+<?php 
+$s1=$_POST["s1"];
+$s2=$_POST["s2"];
+$toplam=0;
+
+while($s1<=$s2){
+	$toplam=$toplam+$s1;
+	$s1++;
+	}
+echo $toplam
+
+?>
+</body>
+</html>
